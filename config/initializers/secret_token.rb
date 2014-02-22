a81992506a439b66f356195fb62e32a04145aa8c
OrlandoLee <zongshengli@ucla.edu>
2014-02-21 23:08:25 -0800

    initialize pawClinic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PawClinic::Application.config.secret_key_base = 'e5c120196f2d65bcc698db216322cabb7968f6dab136f0a0333430b587ba74498c31bf93d3bb8c7d6520b739a8079c1e545f0d9eb12b611f7baaf944b0aee875'
