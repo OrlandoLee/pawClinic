377576a362996c98f79a2af4209ba4e374a705a4
OrlandoLee <zongshengli@ucla.edu>
2014-02-22 15:15:03 -0800

    finishing static page

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-PawClinic::Application.config.secret_key_base = 'e5c120196f2d65bcc698db216322cabb7968f6dab136f0a0333430b587ba74498c31bf93d3bb8c7d6520b739a8079c1e545f0d9eb12b611f7baaf944b0aee875'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+PawClinic::Application.config.secret_key_base = secure_token
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 PawClinic::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
   resources :answers
 
   resources :questions
